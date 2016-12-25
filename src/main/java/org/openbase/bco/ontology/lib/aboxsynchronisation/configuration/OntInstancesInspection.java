b96c333147974c911c170f682ebc49e0daa2e330
agatting <agatting@techfak.uni-bielefeld.de>
2016-12-25 18:21:55 +0100

    changed arrayList to hashSet, extended abox configuration part

--- src/main/java/org/openbase/bco/ontology/lib/aboxsynchronisation/configuration/OntInstancesInspection.java
+++ src/main/java/org/openbase/bco/ontology/lib/aboxsynchronisation/configuration/OntInstancesInspection.java
@@ -23,7 +23,6 @@ import org.apache.jena.ontology.OntClass;
 import org.apache.jena.ontology.OntModel;
 import org.apache.jena.util.iterator.ExtendedIterator;
 import org.openbase.bco.ontology.lib.ConfigureSystem;
-import org.openbase.bco.ontology.lib.DataPool;
 import org.openbase.bco.registry.unit.lib.UnitRegistry;
 import org.openbase.jul.exception.CouldNotPerformException;
 import org.openbase.jul.exception.printer.ExceptionPrinter;
@@ -34,8 +33,8 @@ import rst.domotic.service.ServiceTemplateType.ServiceTemplate.ServiceType;
 import rst.domotic.state.EnablingStateType;
 import rst.domotic.unit.UnitConfigType.UnitConfig;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * Created by agatting on 20.12.16.
@@ -52,22 +51,30 @@ public class OntInstancesInspection {
      */
     public OntInstancesInspection(final OntModel ontModel) {
 
-        final DataPool dataPool = new DataPool();
-        final UnitRegistry unitRegistry = dataPool.getUnitRegistry();
-
-        final List<UnitConfig> unitConfigList = inspectionOfUnits(ontModel, unitRegistry);
-        final List<ServiceType> serviceTypeList = inspectionOfServiceTypes(ontModel);
+//        final DataPool dataPool = new DataPool();
+//        final UnitRegistry unitRegistry = dataPool.getUnitRegistry();
+//
+//        final List<UnitConfig> unitConfigList = inspectionOfUnits(ontModel, unitRegistry);
+//        final List<ServiceType> serviceTypeList = inspectionOfServiceTypes(ontModel);
     }
 
-    private List<UnitConfig> inspectionOfUnits(final OntModel ontModel, final UnitRegistry unitRegistry) {
+    /**
+     * Method compares the units of the registry with the actual units in the ontology model. Missing units are listed.
+     *
+     * @param ontModel The actual ontology model.
+     * @param unitRegistry The actual registry.
+     *
+     * @return A set of missing unitConfigs (units) in the actual ontology model.
+     */
+    protected Set<UnitConfig> inspectionOfUnits(final OntModel ontModel, final UnitRegistry unitRegistry) {
 
-        final List<UnitConfig> missingUnitConfigList = new ArrayList<>();
-        List<Individual> ontUnitIndList = new ArrayList<>(); //Ind: individual
+        final Set<UnitConfig> missingUnitConfigSet = new HashSet<>();
+        Set<Individual> ontUnitIndSet = new HashSet<>(); //Ind: individual
 
         // preparation: get all individuals of the class "Unit" which are currently in the model
         final OntClass ontClassUnit = ontModel.getOntClass(ConfigureSystem.NS + ConfigureSystem.UNIT_SUPERCLASS);
         if (ontClassUnit != null) {
-            ontUnitIndList = getIndOfOntSuperclass(ontUnitIndList, ontClassUnit);
+            ontUnitIndSet = getIndOfOntSuperclass(ontUnitIndSet, ontClassUnit);
         }
 
         try {
@@ -77,8 +84,8 @@ public class OntInstancesInspection {
                     final String unitId = unitConfig.getId();
 
                     // list all missing units. Means units, which aren't currently in the model
-                    if (!ontUnitIndList.toString().contains(unitId)) {
-                        missingUnitConfigList.add(unitConfig);
+                    if (!ontUnitIndSet.toString().contains(unitId)) {
+                        missingUnitConfigSet.add(unitConfig);
                     }
                 }
             }
@@ -86,18 +93,18 @@ public class OntInstancesInspection {
             ExceptionPrinter.printHistory("Could not perform unitRegistry in method "
                     + e.getStackTrace()[0].getMethodName(), e, LOGGER);
         }
-        return missingUnitConfigList;
+        return missingUnitConfigSet;
     }
 
-    private List<ServiceType> inspectionOfServiceTypes(final OntModel ontModel) {
+    private Set<ServiceType> inspectionOfServiceTypes(final OntModel ontModel) {
 
-        final List<ServiceType> missingServiceTypeList = new ArrayList<>();
-        List<Individual> ontServiceTypeIndList = new ArrayList<>(); //Ind: individual
+        final Set<ServiceType> missingServiceTypeSet = new HashSet<>();
+        Set<Individual> ontServiceTypeIndSet = new HashSet<>(); //Ind: individual
 
         // preparation: get all individuals of the class "ProviderService" which are currently in the model
         final OntClass ontClassServiceType = ontModel
                 .getOntClass(ConfigureSystem.NS + ConfigureSystem.PROVIDER_SERVICES_SUPERCLASS);
-        ontServiceTypeIndList = getIndOfOntSuperclass(ontServiceTypeIndList, ontClassServiceType);
+        ontServiceTypeIndSet = getIndOfOntSuperclass(ontServiceTypeIndSet, ontClassServiceType);
 
         // get all serviceTypes (ProviderService) of the registry
         final ServiceType[] serviceTypeArray = ServiceTemplateType.ServiceTemplate.ServiceType.values();
@@ -106,15 +113,17 @@ public class OntInstancesInspection {
             final String serviceType = serviceTypeElement.toString();
 
             // list all missing serviceTypes. Means serviceTypes, which aren't currently in the model
-            if (!ontServiceTypeIndList.toString().contains(serviceType)) {
-                missingServiceTypeList.add(serviceTypeElement);
+            if (!ontServiceTypeIndSet.toString().contains(serviceType)) {
+                missingServiceTypeSet.add(serviceTypeElement);
             }
         }
-        return missingServiceTypeList;
+        return missingServiceTypeSet;
     }
 
     // get a list with all individuals of a superclass via recursion
-    private List<Individual> getIndOfOntSuperclass(final List<Individual> individualList, final OntClass ontClass) {
+    private Set<Individual> getIndOfOntSuperclass(final Set<Individual> individualSet, final OntClass ontClass) {
+
+        //TODO remove namespace (and prefix) via prefix
 
         final ExtendedIterator instanceExIt;
         if (ontClass.hasSubClass()) {
@@ -122,41 +131,41 @@ public class OntInstancesInspection {
             // case: class has subclass and individuals
             instanceExIt = ontClass.listInstances();
             while (instanceExIt.hasNext()) {
-                individualList.add((Individual) instanceExIt.next());
+                individualSet.add((Individual) instanceExIt.next());
             }
 
             // goto next (sub-)class
             final ExtendedIterator<OntClass> ontClassExIt = ontClass.listSubClasses();
             while (ontClassExIt.hasNext()) {
                 final OntClass ontSubClass = ontClassExIt.next();
-                getIndOfOntSuperclass(individualList, ontSubClass);
+                getIndOfOntSuperclass(individualSet, ontSubClass);
             }
         } else {
 
             // class has no subclass(es) anymore. add individuals to list
             instanceExIt = ontClass.listInstances();
             while (instanceExIt.hasNext()) {
-                individualList.add((Individual) instanceExIt.next());
+                individualSet.add((Individual) instanceExIt.next());
             }
         }
-        return individualList;
+        return individualSet;
     }
 
     /**
      * Method delivers all subclasses of the given superclass via recursion.
      *
-     * @param ontClassList The (empty) list to itemize the ontClasses.
+     * @param ontClassSet The (empty) set to itemize the ontClasses.
      * @param ontSuperClass The superclass.
      * @param inclusiveSuperclass Result list keeps superclass or not.
      *
      * @return The list with ontClasses.
      */
-    public List<OntClass> getAllSubclassesOfOntSuperclass(final List<OntClass> ontClassList, final OntClass
+    public Set<OntClass> getAllSubclassesOfOntSuperclass(final Set<OntClass> ontClassSet, final OntClass
             ontSuperClass, final boolean inclusiveSuperclass) {
 
         // add initial superclass
         if (inclusiveSuperclass) {
-            ontClassList.add(ontSuperClass);
+            ontClassSet.add(ontSuperClass);
         }
 
         // get all subclasses of current superclass
@@ -166,12 +175,12 @@ public class OntInstancesInspection {
         // add subclass(es) and if subclass has subclass(es) goto next layer via recursion
         while (ontClassExIt.hasNext()) {
             final OntClass ontClass = (OntClass) ontClassExIt.next();
-            ontClassList.add(ontClass);
+            ontClassSet.add(ontClass);
 
             if (ontSuperClass.hasSubClass()) {
-                getAllSubclassesOfOntSuperclass(ontClassList, ontClass, false);
+                getAllSubclassesOfOntSuperclass(ontClassSet, ontClass, false);
             }
         }
-        return ontClassList;
+        return ontClassSet;
     }
 }
--- src/main/java/org/openbase/bco/ontology/lib/aboxsynchronisation/configuration/OntInstancesMapping.java
+++ src/main/java/org/openbase/bco/ontology/lib/aboxsynchronisation/configuration/OntInstancesMapping.java
@@ -18,7 +18,20 @@
  */
 package org.openbase.bco.ontology.lib.aboxsynchronisation.configuration;
 
+import org.apache.jena.ontology.OntClass;
 import org.apache.jena.ontology.OntModel;
+import org.openbase.bco.ontology.lib.ConfigureSystem;
+import org.openbase.bco.ontology.lib.DataPool;
+import org.openbase.bco.ontology.lib.TripleArrayList;
+import org.openbase.bco.registry.unit.lib.UnitRegistry;
+import rst.domotic.unit.UnitConfigType.UnitConfig;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 /**
  * Created by agatting on 23.12.16.
@@ -28,16 +41,57 @@ public class OntInstancesMapping extends OntInstancesInspection {
     /**
      * Constructor for OntInstancesInspection.
      *
-     * @param ontModel ontModel is the ontology model.
+     * @param ontModel The actual ontology model.
      */
     public OntInstancesMapping(final OntModel ontModel) {
         super(ontModel);
 
-//        List<TripleArrayList> tripleArrayLists = new ArrayList<>();
+        final DataPool dataPool = new DataPool();
+        final UnitRegistry unitRegistry = dataPool.getUnitRegistry();
 
-//        tripleArrayLists.add(new TripleArrayList("a", "b", "c"));
-//        tripleArrayLists.add(new TripleArrayList("d", "e", "f"));
+        final Set<UnitConfig> unitConfigSet = inspectionOfUnits(ontModel, unitRegistry);
 
+        final OntClass ontClass = ontModel.getOntClass(ConfigureSystem.NS + ConfigureSystem.UNIT_SUPERCLASS);
+        Set<OntClass> ontClassSet = new HashSet<>();
+        ontClassSet = getAllSubclassesOfOntSuperclass(ontClassSet, ontClass, true);
+
+        getIndPositionInOnt(ontClassSet, unitConfigSet);
+    }
+
+    private void getIndPositionInOnt(final Set<OntClass> ontClassSet, final Set<UnitConfig> unitConfigSet) {
+
+        final Map<String, String> unitTypeUnitIdMap = new HashMap<>();
+
+        // list all unitTypes and their unitIds of the unitConfigSet in a hashMap
+        for (final UnitConfig unitConfig : unitConfigSet) {
+            String unitType = unitConfig.getType().toString().toLowerCase();
+            unitType = unitType.replaceAll("[^\\p{Alpha}]", "");
+
+            unitTypeUnitIdMap.put(unitType, unitConfig.getId());
+        }
+
+        final List<TripleArrayList> tripleArrayLists = new ArrayList<>();
+
+        for (OntClass ontClass : ontClassSet) {
+            final String ontClassName = ontClass.getLocalName().toLowerCase();
+
+            for (final Map.Entry<String, String> entry : unitTypeUnitIdMap.entrySet()) {
+                if (entry.getKey().equals(ontClassName)) {
+                    tripleArrayLists.add(new TripleArrayList("I_" + entry.getValue(), "a", ontClass.getLocalName()));
+                }
+            }
+
+
+            //TODO differentiate between door, window, passage and region, tile, zone
+
+            //TODO Prefix
+//            if (className.startsWith("u_")) {
+//                System.out.println(className);
+//                className.getChars(2, className.length(), className.toCharArray(), 0);
+//                System.out.println(className);
+//            }
+
+        }
     }
 
 }
